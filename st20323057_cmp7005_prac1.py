2518c92809ca3720387cce07ef46bc0e24c22a82
kingfantasy <135114858+kingfantasy@users.noreply.github.com>
2025-05-12 14:17:30 +0100

    Update st20323057_cmp7005_prac1.py

--- st20323057_cmp7005_prac1.py
+++ st20323057_cmp7005_prac1.py
@@ -418,11 +418,11 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
- from sklearn.model_selection import train_test_split
- from sklearn.linear_model import LinearRegression
- from sklearn.metrics import mean_squared_error, r2_score
- from sklearn.preprocessing import StandardScaler
- from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import GradientBoostingRegressor
  
  #Page Config & Style
  st.set_page_config(page_title="Air Quality Dashboard", layout="wide")
